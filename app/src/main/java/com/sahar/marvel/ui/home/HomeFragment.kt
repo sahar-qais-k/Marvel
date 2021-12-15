5dad44fa8a60242fe31e02b96eec3d6e931e48d0
Sahar Qais <sahar.qais.k@gmail.com>
2021-12-15 22:17:18 +0300

    feat: store data in firebase

--- app/src/main/java/com/sahar/marvel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sahar/marvel/ui/home/HomeFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.sahar.marvel.databinding.FragmentHomeBinding
 import com.sahar.marvel.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,13 +21,18 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        // ...
+        var database: DatabaseReference = Firebase.database.reference
         initAdapter()
-//        viewModel.characters.observe(this, {
+        viewModel.characters.observe(this, {
+//            database.setValue(it.toData())
+            database.child("Characters").child(it.toData()?.get(0)?.id.toString()).setValue(it.toData())
+
 //             Log.i("resultb",  it[0].imageUrl)
-//        })
+        })
     }
     private fun initAdapter() {
         binding.charactersRecycler.adapter = CharacterAdapter(viewModel.characters.value?.toData() ?: emptyList(), viewModel)
+
     }
 }
